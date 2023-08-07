23071e1dc5f6c93432301c05174fa5fd21239408
YeoJiSu <duwltn1301@naver.com>
2023-08-07 13:27:21 +0900

    chore: change debug location

--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/yeojisu/Desktop/No-Crime/api/venv"
+set -gx VIRTUAL_ENV "/Users/yeojisu/Documents/No-Crime/backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
