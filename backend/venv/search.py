293076aac0c2c9bad684250bfda65c350027bf4e
YeoJiSu <duwltn1301@naver.com>
2023-08-07 15:44:45 +0900

    feat: 시/군/구 조회 기능 제작

--- backend/venv/search.py
+++ backend/venv/search.py
@@ -8,8 +8,16 @@ class AdministrativeDistrict(Resource):
         keys = [key for key in distinct_1]
         return {'도/특별시/광역시':keys}
     
-    # # 시/군/구 조회
-    # def get(self):
+    # 시/군/구 조회
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('도/특별시/광역시')
+        args = parser.parse_args()
+        argument = args['도/특별시/광역시']
+        
+        distinct_1= json.load(open('model/district.json', 'rb'))
+        data = distinct_1[argument]
+        return {'data': data}
         
 
 
