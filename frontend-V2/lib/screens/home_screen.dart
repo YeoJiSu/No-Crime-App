1f0b26be3acb58b8f431488f475b56ebb0d26700
Jisu Yeo <duwltn1301@naver.com>
2023-09-02 19:04:39 +0900

    feat: SOS 버튼 우측 상단에 배치 및 112 신고 기능

--- frontend-V2/lib/screens/home_screen.dart
+++ frontend-V2/lib/screens/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:nocrime/screens/predict_here_screen.dart';
 import 'package:nocrime/screens/predict_other_screen.dart';
 import 'package:nocrime/screens/search_crime_screen.dart';
 import 'package:nocrime/screens/white_paper_screen.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -60,187 +61,155 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: const Color.fromRGBO(29, 29, 37, 1),
       body: Padding(
         padding: const EdgeInsets.all(20),
-        child: Stack(
+        child: Column(
           children: [
-            Column(
-              children: [
-                const SizedBox(
-                  height: 30,
-                ),
-                const firstAppBar(),
-                const SizedBox(
-                  height: 10,
-                ),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: List.generate(buttonTitles.length, (index) {
-                      return GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            _selectedIndex = index;
-                          });
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 8, vertical: 8),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Text(
-                                buttonTitles[index],
-                                style: TextStyle(
-                                  color: _selectedIndex == index
-                                      ? Colors.white
-                                      : const Color.fromRGBO(206, 206, 206, 1),
-                                  fontSize: 17.5,
-                                  fontWeight: _selectedIndex == index
-                                      ? FontWeight.bold
-                                      : FontWeight.normal,
-                                ),
-                              ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              AnimatedContainer(
-                                duration: const Duration(
-                                    milliseconds: 300), // Animation duration
-                                height: 3,
-                                width: 80,
-                                alignment: _selectedIndex == index
-                                    ? Alignment.center
-                                    : Alignment
-                                        .centerLeft, // Adjust alignment based on selection
-                                color: _selectedIndex == index
-                                    ? Colors.white
-                                    : Colors.transparent,
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    }),
-                  ),
-                ),
-                Expanded(
-                  child: SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 15,
-                            horizontal: 20,
-                          ),
-                          decoration: BoxDecoration(
-                            border: const Border.fromBorderSide(BorderSide(
-                                color: Color.fromRGBO(64, 64, 64, 1),
-                                width: 1)), // 테두리 스타일과 색상 설정
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                showBigTitle(),
-                                style: const TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                  letterSpacing: 1.0,
-                                ),
-                              ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              Text(
-                                showSmallTitle(),
-                                style: const TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.white,
-                                  letterSpacing: 0.9,
-                                  height: 1.5,
-                                ),
-                              ),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  const SizedBox(
-                                    height: 1,
-                                  ),
-                                  if (_selectedIndex == 0)
-                                    ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                        backgroundColor: const Color.fromRGBO(
-                                            131, 131, 255, 0.5),
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(20.0),
-                                        ),
-                                      ),
-                                      onPressed: () {
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  const PredictOtherScreen()),
-                                        );
-                                      },
-                                      child: const Text(
-                                        '다른 지역 예측하기 >',
-                                        style: TextStyle(
-                                          fontSize: 15,
-                                        ),
-                                      ),
-                                    ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 30,
-                        ),
-                        if (_selectedIndex == 0) const PredictHereScreen(),
-                        if (_selectedIndex == 1) const SearchCrimeScreen(),
-                        if (_selectedIndex == 2) const CctvMapScreen(),
-                        if (_selectedIndex == 3) const WhitePaperScreen(),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
+            const SizedBox(
+              height: 30,
             ),
-            Positioned(
-              bottom: 50.0, // 아래 여백 조정
-              right: 16.0, // 우측 여백 조정
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  maximumSize: const Size(100, 100),
-                  foregroundColor: Colors.black,
-                  textStyle: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                  backgroundColor: const Color.fromRGBO(128, 255, 179, 1),
-                  shape: RoundedRectangleBorder(
-                    side: const BorderSide(
-                      width: 2,
-                      color: Color.fromARGB(255, 115, 166, 255),
+            const firstAppBar(),
+            const SizedBox(
+              height: 10,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: List.generate(buttonTitles.length, (index) {
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _selectedIndex = index;
+                      });
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 8),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            buttonTitles[index],
+                            style: TextStyle(
+                              color: _selectedIndex == index
+                                  ? Colors.white
+                                  : const Color.fromRGBO(206, 206, 206, 1),
+                              fontSize: 17.5,
+                              fontWeight: _selectedIndex == index
+                                  ? FontWeight.bold
+                                  : FontWeight.normal,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          AnimatedContainer(
+                            duration: const Duration(
+                                milliseconds: 300), // Animation duration
+                            height: 3,
+                            width: 80,
+                            alignment: _selectedIndex == index
+                                ? Alignment.center
+                                : Alignment
+                                    .centerLeft, // Adjust alignment based on selection
+                            color: _selectedIndex == index
+                                ? Colors.white
+                                : Colors.transparent,
+                          ),
+                        ],
+                      ),
                     ),
-                    borderRadius: BorderRadius.circular(50.0), // 버튼을 둥글게 만듭니다.
-                  ), // 버튼 텍스트 색상을 변경합니다.
-                  elevation: 8.0, // 버튼의 그림자를 추가합니다.
+                  );
+                }),
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 15,
+                        horizontal: 20,
+                      ),
+                      decoration: BoxDecoration(
+                        border: const Border.fromBorderSide(BorderSide(
+                            color: Color.fromRGBO(64, 64, 64, 1),
+                            width: 1)), // 테두리 스타일과 색상 설정
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            showBigTitle(),
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              letterSpacing: 1.0,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            showSmallTitle(),
+                            style: const TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                              letterSpacing: 0.9,
+                              height: 1.5,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const SizedBox(
+                                height: 1,
+                              ),
+                              if (_selectedIndex == 0)
+                                ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: const Color.fromRGBO(
+                                        131, 131, 255, 0.5),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20.0),
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              const PredictOtherScreen()),
+                                    );
+                                  },
+                                  child: const Text(
+                                    '다른 지역 예측하기 >',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    if (_selectedIndex == 0) const PredictHereScreen(),
+                    if (_selectedIndex == 1) const SearchCrimeScreen(),
+                    if (_selectedIndex == 2) const CctvMapScreen(),
+                    if (_selectedIndex == 3) const WhitePaperScreen(),
+                  ],
                 ),
-                onPressed: () {
-                  // 버튼을 눌렀을 때 실행할 작업을 추가하세요.
-                },
-                child: const Text('SOS'),
               ),
             ),
           ],
@@ -254,15 +223,16 @@ class firstAppBar extends StatelessWidget {
   const firstAppBar({
     super.key,
   });
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(10),
-      child: const Row(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 10,
+      ),
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text(
+          const Text(
             "No Crime",
             style: TextStyle(
               color: Color.fromRGBO(128, 255, 179, 1),
@@ -270,8 +240,38 @@ class firstAppBar extends StatelessWidget {
               fontWeight: FontWeight.bold,
             ),
           ),
+          ElevatedButton(
+            onPressed: () {
+              // 버튼을 눌렀을 때 실행할 작업을 추가하세요.
+              () async {
+                final url = Uri.parse('tel:112');
+                try {
+                  if (await canLaunchUrl(url)) {
+                    await launchUrl(url);
+                    print("success calling 112");
+                  } else {
+                    print("Can't launch URL");
+                  }
+                } catch (e) {
+                  print(e);
+                }
+              };
+            },
+            style: ElevatedButton.styleFrom(
+              textStyle: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+              foregroundColor: Colors.black,
+              backgroundColor: const Color.fromRGBO(128, 255, 179, 1),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(40.0), // 버튼을 둥글게 만듭니다.
+              ), // 버튼 텍스트 색상을 변경합니다.
+              elevation: 8.0, // 버튼의 그림자를 추가합니다.
+            ),
+            child: const Text('SOS'),
+          )
           // Icon(
-          //   Icons.settings,
+          //   Icons.call,
           //   size: 30,
           //   color: Color.fromRGBO(128, 255, 179, 1),
           // ),
